e41b120992fb289e211a57228c5895cc06da59fe
Wajid Khan <studentlearning146@gmail.com>
2026-04-23 12:24:58 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+# Create DB
+def init_db():
+    conn = sqlite3.connect('database.db')
+    conn.execute('CREATE TABLE IF NOT EXISTS messages (name TEXT, email TEXT, message TEXT)')
+    conn.close()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    conn = sqlite3.connect('database.db')
+    conn.execute("INSERT INTO messages (name,email,message) VALUES (?,?,?)",
+                 (name, email, message))
+    conn.commit()
+    conn.close()
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    init_db()
+    app.run(host='0.0.0.0', port=5000)
